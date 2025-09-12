546603eef2aaaf27931327c77de5312f1e140fcf
Yeison Suarez <yheison0035@gmail.com>
2025-09-12 13:10:20 -0500

    se incluye traduccion

--- src/auth/dto/change-password.dto.ts
+++ src/auth/dto/change-password.dto.ts
@@ -5,6 +5,6 @@ export class ChangePasswordDto {
   currentPassword: string;
 
   @IsString()
-  @MinLength(6)
+  @MinLength(6, { message: 'La nueva contraseña debe tener al menos 6 caracteres' })
   newPassword: string;
 }
