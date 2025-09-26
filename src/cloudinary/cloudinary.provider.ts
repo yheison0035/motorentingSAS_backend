8163a8676fb505ce692c71f2de271f25840eac38
Yeison Suarez <yheison0035@gmail.com>
2025-09-26 18:59:03 -0500

    cambio en cloudinary

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -3,9 +3,7 @@ import { v2 as cloudinary } from 'cloudinary';
 export const CloudinaryProvider = {
   provide: 'CLOUDINARY',
   useFactory: () => {
-    cloudinary.config({
-      CLOUDINARY_URL: process.env.CLOUDINARY_URL,
-    });
+    cloudinary.config({});
     return cloudinary;
   },
 };
