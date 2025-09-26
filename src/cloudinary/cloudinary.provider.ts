1b43cc2a6de197313e04612b79e42c9d227587fb
Yeison Suarez <yheison0035@gmail.com>
2025-09-26 18:29:18 -0500

    se integran validacion de consumo de cloudinary

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -4,9 +4,7 @@ export const CloudinaryProvider = {
   provide: 'CLOUDINARY',
   useFactory: () => {
     cloudinary.config({
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
+      CLOUDINARY_URL: process.env.CLOUDINARY_URL,
     });
     return cloudinary;
   },
